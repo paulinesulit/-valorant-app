194fcaa3ce82f04b3006251f3e9ab727d50e3f10
Pauline <lynnsulit@gmail.com>
2022-10-11 00:24:50 -0400

    added more functions

--- src/components/Agents.js
+++ src/components/Agents.js
@@ -5,6 +5,7 @@ const Agents = () => {
   const [allAgents, setAllAgents] = useState([]);
   const [userAgents, setUserAgents] = useState([]);
   const [randomAgent, setRandomAgent] = useState([]);
+  const [randomAllAgents, setRandomAllAgents] = useState([]);
 
   // API call
   useEffect(() => {
@@ -28,19 +29,30 @@ const Agents = () => {
   // Click to select characters use wants to randomize
   const addAgent = (agent) => {
     const agentObject = {
+      id: agent.uuid,
       name: agent.displayName,
       description: agent.description,
       photo: agent.fullPortrait,
     };
-
     setUserAgents((current) => [...current, agentObject]);
   };
 
   // Randomize agents selected
+
   const randomizeAgent = () => {
     const randomNum = Math.floor(Math.random() * userAgents.length);
     setRandomAgent(userAgents[randomNum]);
   };
+
+  // Randomize all agents if none are selected (randomAgent is empty)
+  const randomizeAllAgents = () => {
+    const randomNum = Math.floor(Math.random() * allAgents.length);
+    setRandomAllAgents(allAgents[randomNum]);
+  };
+
+  // console.log(randomAllAgents);
+
+  // add X to remove selected characters and clear all
 
   return (
     <div>
@@ -48,28 +60,84 @@ const Agents = () => {
         <h2>Agents here</h2>
         {allAgents.map((eachAgent) => {
           return (
-            <div>
+            <div key={eachAgent.uuid}>
               <button
                 onClick={() => {
                   addAgent(eachAgent);
                 }}
               >
+                {/* {addToList ? eachAgent.displayName : "✓"} */}
+                {/* {console.log(addToList, eachAgent.displayName)} */}
+                {/* {console.log(userAgents.name)} */}
                 {eachAgent.displayName}
+                {console.log(userAgents)}
               </button>
             </div>
           );
         })}
+
         <button
-          class="randomBtn"
+          className="randomBtn"
           onClick={() => {
-            randomizeAgent();
+            randomizeAllAgents();
           }}
         >
-          Randomize
+          Randomize All
         </button>
-        {console.log(randomAgent)}
-        <p>{randomAgent.name}</p>
-        <img src={randomAgent.photo} alt="photo of agent" />
+
+        <div>
+          {userAgents.map((selectedAgents) => {
+            return (
+              <div key={selectedAgents.id} className="selectedAgents">
+                <p>{selectedAgents.name}</p>
+              </div>
+            );
+          })}
+
+          <button
+            className="randomBtn"
+            onClick={() => {
+              randomizeAgent();
+            }}
+          >
+            Randomize
+          </button>
+        </div>
+
+        {/* {userAgents.length === 0 ? (
+          <button
+            className="randomBtn"
+            onClick={() => {
+              randomizeAllAgents();
+            }}
+          >
+            Randomize All
+          </button>
+        ) : (
+          <button
+            className="randomBtn"
+            onClick={() => {
+              randomizeAgent();
+            }}
+          >
+            Randomize
+          </button>
+        )} */}
+
+        {/* {console.log(randomAgent)} */}
+
+        {randomAgent.length === 0 ? null : (
+          <div>
+            <p>{randomAgent.name}</p>
+            <img src={randomAgent.photo} alt="photo of agent" />
+          </div>
+        )}
+        {randomAllAgents.length === 0 ? null : (
+          <div>
+            <p>{randomAllAgents.displayName}</p>
+            <img src={randomAllAgents.fullPortrait} alt="photo of agent" />
+          </div>
+        )}
       </section>
     </div>
   );
