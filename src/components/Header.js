d45d8f90a51a57251e8fe5abb43935a13215dfd2
Pauline <lynnsulit@gmail.com>
2022-10-21 23:53:31 -0400

    added refresh when header clicked :sparkles:

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ const Header = () => {
   return (
     <div className="wrapper">
       <header>
-        <h1>Pick Your Valorant Agent!</h1>
+        <a href="/">
+          <h1>Pick Your Valorant Agent!</h1>
+        </a>
         <p>
           A randomizer that will allow you to choose an agent. You can either
           randomize all agents or select which agents you have and randomize
