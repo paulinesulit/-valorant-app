9f517aa33151a8d8caaf6c3114f49633e06ed8c0
Pauline <lynnsulit@gmail.com>
2022-10-24 17:56:37 -0400

    added portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ const Footer = () => {
   return (
     <div className="wrapper footer">
       <footer>
-        <p>Created by Pauline</p>
+        <p>
+          Created by{" "}
+          <a href="https://paulinesulit.com/" target="_blank" rel="noreferrer">
+            Pauline
+          </a>
+        </p>
       </footer>
     </div>
   );
